e9e6390bbf37c8d222756dab9239dafaedc008a6
Cyytrus <antoniopaulo122@gmail.com>
2023-12-15 15:38:58 -0300

    first commit :rocket:

---
+++ lib/opium_livebook.ex
@@ -0,0 +1,18 @@
+defmodule OpiumLivebook do
+  @moduledoc """
+  Documentation for `OpiumLivebook`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OpiumLivebook.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/opium_livebook_test.exs
@@ -0,0 +1,8 @@
+defmodule OpiumLivebookTest do
+  use ExUnit.Case
+  doctest OpiumLivebook
+
+  test "greets the world" do
+    assert OpiumLivebook.hello() == :world
+  end
+end
